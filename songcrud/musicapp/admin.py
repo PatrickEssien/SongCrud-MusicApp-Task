ec7dcb4b1ec2d97507d8f39f6cfcbf00d515351b
Patrick Essien <kwglobalconcept@gmail.com>
2022-10-21 01:56:09 +0100

    Models and Attributes addition

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from.models import artiste, Songs
+# Register  your models here.
+# I registered the models and attributes i created here.
+admin.site.register(artiste)
+admin.site.register(Songs)
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
+from msilib.schema import Class
+from unittest.util import _MAX_LENGTH
 from django.db import models
+# I cretaed the models and attributes for nmy music app
 
 # Create your models here.
+class artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+    def _str_(self) -> str:
+      return self.name
+
+class Songs(models.Model):
+    Title = models.CharField(max_length=50)
+    Date_Released = models.CharField(max_length=50)
+    Likes = models.CharField(max_length=50)
+    artiste_id = models.CharField(max_length=50)
+    def _str_(self) -> str:
+      return self.name
