818efad930f399a766c3d937f61e29aebca79b61
Patrick Essien <kwglobalconcept@gmail.com>
2022-10-24 12:40:28 +0100

    index modified

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from msilib.schema import Class
 from unittest.util import _MAX_LENGTH
 from django.db import models
@@ -5,16 +6,20 @@ from django.db import models
 
 # Create your models here.
 class artiste(models.Model):
+    # creating the attributes for the artiste
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.CharField(max_length=50)
+
     def _str_(self) -> str:
       return self.name
 
 class Songs(models.Model):
+    # creating the attributes for the songs
     Title = models.CharField(max_length=50)
     Date_Released = models.CharField(max_length=50)
     Likes = models.CharField(max_length=50)
     artiste_id = models.CharField(max_length=50)
+    User = models.ForeignKey( artiste, on_delete=models.CASCADE)
     def _str_(self) -> str:
       return self.name
